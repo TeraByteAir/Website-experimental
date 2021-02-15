4f064b113b7b86d148be07f19309df6a4f0b48a5
TeraByteAir <66703937+TeraByteAir@users.noreply.github.com>
2021-02-15 10:47:31 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+function clickFunction() {
+    alert("Wow this is a message made by script.js")
+}
+
+function renderTime() {
+    var mydate = new Date();
+    var year = mydate.getFullYear();
+        if (year < 1000) {
+            year += 1900
+        }
+    var day = mydate.getDay();
+    var month = mydate.getMonth();
+    var daym = mydate.getDay();
+    var dayarray = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+    var montharray = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December",);
+
+    var currentTime = new Date();
+    var h = currentTime.getHours();
+    var m = currentTime.getMinutes();
+    var s = currentTime.getSeconds();
+        if (h == 24) {
+            h = 0;
+        } else if(h > 12) {
+        h = h - 0;
+        }
+
+        if (h < 10) {
+            h = "0" + h;
+        }
+ 
+        if (m < 10) {
+            m = "0" + m;
+        }
+
+        if (s < 10){
+            s = "0" + s;
+        }   
+     var myClock = document.getElementById("clockDisplay");
+     myClock.textContent= " " + daym+ " "+ montharray[month]+ " " +year+ " | "+ h+ ":" + m + ":" +s;
+     myClock.innerText=  " " +daym+ " "+ montharray[month]+ " " +year+ " | "+ h+ ":" + m + ":" +s;
+
+     setTimeout("renderTime()", 1000);
+}
+
+renderTime();
+
